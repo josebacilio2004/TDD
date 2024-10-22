7c5ee2ad5b768bff47a856b46fa2a4a0d6aa5f4c
Jose Bacilio <74934503@continental.edu.pe>
2024-10-22 15:00:54 -0500

    clase Calculadora Estadistica def agregar elemento

--- src/logica/calculadora.py
+++ src/logica/calculadora.py
@@ -6,3 +6,12 @@ class NoSePuedeCalcular(Exception):
     """Excepción lanzada cuando no se puede calcular la media o desviación estándar."""
     pass
 
+class CalculadoraEstadistica:
+    def __init__(self):
+        self.elementos = []
+
+    def agregar_elemento(self, elemento):
+        if not isinstance(elemento, (int, float)):
+            raise TypeError("Solo se pueden agregar elementos numéricos.")
+        self.elementos.append(elemento)
+
