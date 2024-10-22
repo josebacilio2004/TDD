bbc437a4f53615344d2234019029b186d55604ea
Jose Bacilio <74934503@continental.edu.pe>
2024-10-22 15:01:33 -0500

    clase Calculadora Estadistica def calcular desviacion estandar

--- src/logica/calculadora.py
+++ src/logica/calculadora.py
@@ -20,3 +20,13 @@ class CalculadoraEstadistica:
             raise NoSePuedeCalcular("La lista está vacía, no se puede calcular la media.")
         return sum(self.elementos) / len(self.elementos)
 
+    def calcular_desviacion_estandar(self):
+        if not self.elementos:
+            raise NoSePuedeCalcular("La lista está vacía, no se puede calcular la desviación estándar.")
+        if len(self.elementos) == 1:
+            return 0.0  # La desviación estándar de un solo elemento es 0
+        media = self.calcular_media()
+        varianza = sum((x - media) ** 2 for x in self.elementos) / len(self.elementos)
+        return math.sqrt(varianza)
+
+
